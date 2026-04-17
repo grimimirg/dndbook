4852e50668f5dac0accd156989be5258fbff0a7c
Andrea Grimandi <andrea.grimandi1@protonmail.ch>
2026-04-17 15:31:19 +0200

    feat(dndbook) [main] usage of Eventlet instead of werkzeug

--- be/app.py
+++ be/app.py
@@ -1,9 +1,10 @@
-from app import create_app, db, socketio
+import eventlet
+
+eventlet.monkey_patch()
+
+from app import create_app, socketio
 
 flask_app = create_app()
 
-# Import socketio events
-import app.socketio_events
-
 if __name__ == '__main__':
-    socketio.run(flask_app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(flask_app, host='0.0.0.0', port=5000, debug=False)
